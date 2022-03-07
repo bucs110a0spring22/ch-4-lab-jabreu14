296948c086499c18de925f986acbdc16747a6a90
Jeffrey Abreu <98533757+jabreu14@users.noreply.github.com>
2022-03-07 15:40:42 +0000

    None

--- README.md
+++ README.md
@@ -8,18 +8,18 @@
 _Replace anything surrounded by the `< >` symbols._
 
 ## SUMMARY:
- < Provide a short description of your program's functionality. No more than a couple sentences. >
+ My program is able to draw sine, cosine, and tangent curves using functions and for loops. It also draws an x and y axis and fits the graph to a certain range and domain.
 
 ## GRACE DAYS
-Grace days used for this assignment: < # >
+Grace days used for this assignment: 0
 
-Grace days remaining: < # >/5
+Grace days remaining: 5/5
 
 ## KNOWN BUGS AND INCOMPLETE PARTS:
- < What parts of the project you were not able to complete >
+None
 
 ## REFERENCES:
- < List any outside resources used >
+None
 
 ## MISCELLANEOUS COMMENTS:
- < Anything you would like the grader to know >
+None
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import turtle
-
+import math
 ########### Your Code here ##############
 # You should only have functions here
 # If you have anything outside of a function, 
@@ -7,20 +7,61 @@ import turtle
 # and should review how they work or ask for help
 
 
+def drawSineCurve(myturtle=None):
+  for i in range (-360, 360):
+    num = math.sin(math.radians(i))
+    print(num)
+    myturtle.goto(i,num)
+
+def drawTangentCurve(myturtle=None):
+  myturtle.up()
+  myturtle.goto(-360,0)
+  myturtle.down()
+  for i in range (-360, 360,):
+    num = math.tan(math.radians(i))
+    myturtle.goto(i,num)
+
+def drawCosineCurve(myturtle=None):
+  myturtle.up()
+  myturtle.goto(-360,1)
+  myturtle.down()
+  for i in range (-360, 360):
+    num = math.cos(math.radians(i))
+    myturtle.goto(i,num)
+
+def setupWindow(mywindow=None):
+  mywindow.setworldcoordinates(-360,-1,360,1)
+  mywindow.bgcolor("light blue")
+
+
+
+def setupAxis(myturtle=None):
+  myturtle.goto(0,1)
+  myturtle.goto(0,-1)
+  myturtle.goto(0,0)
+  myturtle.goto(360,0)
+  myturtle.goto(-360,0)
+  
+  
+  
+
+
+ 
+     
 
 
 
 
 
 
-##########  Do Not Alter Any Code Past Here ########
+#########  Do Not Alter Any Code Past Here ########
 def main():
     #Part A
     wn = turtle.Screen()
     wn.tracer(5)
     dart = turtle.Turtle()
-    dart.speed(0)
-    drawSineCurve(dart)
+    dart.speed(1)
+    # drawSineCurve(dart)
 
     #Part B
     setupWindow(wn)
